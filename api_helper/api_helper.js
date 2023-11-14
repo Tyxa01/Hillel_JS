458c3db186db58f11920708f0cb8cc7ff736cbaa
Artour <tyxa2017@gmail.com>
2023-11-14 12:30:42 +0200

    commit_for_prepare

---
+++ api_helper/api_helper.js
@@ -0,0 +1,17 @@
+import fetch from 'node-fetch'
+
+export async function get(url){
+    const response = await fetch(url);
+    return await response.json();
+}
+
+
+export async function post(url, body) {
+    const response = await fetch(url, {
+      method: "post",
+      body: JSON.stringify(body),
+      headers: { "Content-Type": "application/json" },
+    });
+  
+    return response;
+  }
